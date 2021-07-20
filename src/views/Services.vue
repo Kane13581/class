b90c1d29986b3f91357de7617922a35687cc6d3d
Istrate-Andrei <56413983+Kane13581@users.noreply.github.com>
2021-07-20 13:35:04 +0300

    some repairs made

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -6,18 +6,14 @@
     <div v-else class="flex h-80 lg:h-96 justify-around md:justify-center items-center">
       <div class="flex 2xl:mt-32 2xl:border-8 md:mr-8 lg:mr-16 xl:mr-20 justify-center items-center">
         <button @click="showServicesM"
-                class="origin-left bg-gray-300 transform border-8 border-double border-black hover:skew-y-12 hover:bg-greenColor text-7xl rounded-md w-32 md:w-48 md:h-28 lg:w-52 lg:h-40 2xl:w-60 2xl:h-80  text-black font-serif">
-          M
-          <br>
-          <p class="absolute invisible 2xl:visible right-0 bottom-24">.</p>
+                class="bg-gray-300 border-8 border-double border-black hover:bg-greenColor text-3xl rounded-md w-32 md:w-48 md:h-28 lg:w-52 lg:h-40 2xl:w-60 2xl:h-80  text-black font-serif">
+          Grigorescu
         </button>
       </div>
       <div class="flex 2xl:mt-32 2xl:border-8 justify-center items-center">
         <button @click="showServicesF"
-                class="origin-right bg-gray-300 transform hover:bg-greenColor hover:-skew-y-12 text-7xl border-8 border-double border-black rounded-md w-32 md:w-48 md:h-28 lg:w-52 lg:h-40 2xl:w-60 2xl:h-80 text-black font-serif">
-          F
-          <br>
-          <p class="absolute invisible 2xl:visible left-0 bottom-24">.</p>
+                class="bg-gray-300 hover:bg-greenColor text-3xl border-8 border-double border-black rounded-md w-32 md:w-48 md:h-28 lg:w-52 lg:h-40 2xl:w-60 2xl:h-80 text-black font-serif">
+         Marasti
         </button>
       </div>
     </div>
@@ -25,28 +21,40 @@
       <div class="flex justify-center">
       <div v-if="showF" class="flex flex-col w-full md:w-2/3 lg:w-2/4 xl:w-2/5 justify-center text-white p-4 py-12">
         <button @click="showTunsVopsit" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Tuns - Vopsit</button>
-        <div v-if="tunsVopsit" class="flex hover:bg-greenColor rounded-full justify-center text-justify font-serif" v-for="(service6, index6) in services6" :key="index6">
+        <div v-if="tunsVopsit">
+        <div  class="flex hover:bg-greenColor rounded-full justify-center text-justify font-serif" v-for="(service6, index6) in services6" :key="index6">
           {{ `${service6.service} - ${service6.price}` }}
         </div>
+        </div>
         <button @click="showPacheteF" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Pachete</button>
-        <div v-if="pacheteF" class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service5, index5) in services5" :key="index5">
+       <div v-if="pacheteF">
+        <div  class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service5, index5) in services5" :key="index5">
           {{ `${service5.service} - ${service5.price}` }}
         </div>
+        </div>
         <button @click="showEpilareLaserF" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Epilare Laser</button>
-        <div v-if="epilareLaserF" class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service7, index7) in services7" :key="index7">
+        <div v-if="epilareLaserF" >
+        <div class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service7, index7) in services7" :key="index7">
           {{ `${service7.service} - ${service7.price}` }}
         </div>
+        </div>
         <button @click="showPachetEpilareF" class="text-2xl  mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Pachete Epilare Laser</button>
-        <div v-if="pachetEpilareF" class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service4, index4) in services4" :key="index4">
+        <div v-if="pachetEpilareF" >
+        <div class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service4, index4) in services4" :key="index4">
           {{ `${service4.service} - ${service4.price}` }}
         </div>
+        </div>
         <button @click="showCosmetica" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Cosmetica - Epilare Ceara</button>
-        <div v-if="cosmetica" class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service8, index8) in services8" :key="index8">
+        <div v-if="cosmetica" >
+        <div class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service8, index8) in services8" :key="index8">
           {{ `${service8.service} - ${service8.price}` }}
         </div>
+        </div>
         <button @click="showEpilareZahar" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Epilare Pasta Zahar</button>
-        <div v-if="epilareZahar" class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service9, index9) in services9" :key="index9">
+        <div v-if="epilareZahar" >
+        <div class="flex hover:bg-greenColor rounded-full justify-center font-serif" v-for="(service9, index9) in services9" :key="index9">
           {{ `${service9.service} - ${service9.price}` }}
+        </div>
         </div>
       </div>
       </div>
@@ -54,20 +62,28 @@
       <div class="flex justify-center">
         <div v-if="showM" class="flex flex-col w-full md:w-2/3 lg:w-2/4 xl:w-2/5 justify-center text-white p-4 py-12">
           <button @click="showGrigorescu" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Tuns - Grigorescu</button>
-          <div v-if="grigorescu" class="flex justify-center font-serif " v-for="(service, index) in services1" :key="index">
+          <div v-if="grigorescu" >
+          <div class="flex hover:bg-greenColor rounded-full justify-center font-serif " v-for="(service, index) in services1" :key="index">
              {{ `${service.service} - ${service.price}` }}
           </div>
+          </div>
           <button @click="showMarasti" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Tuns - Marasti</button>
-          <div v-if="marasti" class="flex justify-center font-serif " v-for="(service2, index2) in services2" :key="index2">
+          <div v-if="marasti" >
+          <div class="flex hover:bg-greenColor rounded-full justify-center font-serif " v-for="(service2, index2) in services2" :key="index2">
            {{ `${service2.service} - ${service2.price}` }}
           </div>
+          </div>
           <button @click="showEpilareLaserM" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Epilare Laser</button>
-          <div v-if="epilareLaserM" class="flex justify-center font-serif " v-for="(service3, index3) in services3" :key="index3">
+          <div v-if="epilareLaserM" >
+          <div class="flex hover:bg-greenColor rounded-full justify-center font-serif " v-for="(service3, index3) in services3" :key="index3">
             {{ `${service3.service} - ${service3.price}` }}
           </div>
+          </div>
           <button @click="showPachetEpilareM" class="text-2xl mb-1 border-2 p-1 hover:bg-green-300 hover:text-black hover:border-greenColor bg-greenColor focus:outline-none rounded-xl">Pachet Epilare Laser</button>
-          <div v-if="pachetEpilareM" class="flex justify-center text-justify font-serif " v-for="(service4, index4) in services4" :key="index4">
+          <div v-if="pachetEpilareM" >
+          <div class="flex hover:bg-greenColor rounded-full justify-center text-justify font-serif " v-for="(service4, index4) in services4" :key="index4">
             {{ `${service4.service} - ${service4.price}` }}
+          </div>
           </div>
         </div>
       </div>
