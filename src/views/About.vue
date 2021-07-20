10fbaac4d7909979cfc8b0531add5a69ddb0ff0c
Istrate-Andrei <56413983+Kane13581@users.noreply.github.com>
2021-07-20 14:45:37 +0300

    about page update 2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about flex justify-center">
     <div class="border-8 border-double p-8 w-3/4 mt-20">
-      <h1 class="text-white font-serif md:text-2xl text-center">Salon Class Beauty Barber este locul unde visele tale devin inspiratia noastra! Stilul, eleganta, pasiunea si profesionalismul, sunt cuvintele ce caracterizeaza salonul nostru de infrumusetare.</h1>
+      <h1 class="text-white font-serif md:text-2xl text-center">Salon Class Barber&Beauty este locul unde visele tale devin inspiratia noastra! Stilul, eleganta, pasiunea si profesionalismul, sunt cuvintele ce caracterizeaza salonul nostru de infrumusetare.</h1>
     </div>
   </div>
 </template>
